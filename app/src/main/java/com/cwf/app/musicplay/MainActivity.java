d8932625974ddace560ffa2ab596c9c6d36c5f86
n-240 <237142681@qq.com>
2015-11-16 14:08:18 +0800

    Initial commit

---
+++ app/src/main/java/com/cwf/app/musicplay/MainActivity.java
@@ -0,0 +1,326 @@
+package com.cwf.app.musicplay;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private String SPTAG = "play_id";
+    private ListView listView;
+    private ArrayList<MusicLoader.MusicInfo> musicInfos;
+    private MediaPlayer mediaPlayer;
+    private ImageView previous, play, next;
+    private int playId;
+    private SeekBar seekBar;
+    private TextView playnow, playtime;
+    private boolean isPlaying = false;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        fab.setVisibility(View.GONE);
+        playId = (int) SPUtils.get(this, SPTAG, 0);
+        initList();
+        initPlayView();
+        TelephonyManager telephonyManger =(TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManger.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+
+    private PhoneStateListener phoneStateListener = new PhoneStateListener(){
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch(state) {
+                case TelephonyManager.CALL_STATE_IDLE:
+                    System.out.println("挂断");
+                    if(mediaPlayer!=null && isPlaying) {
+                        mediaPlayer.start();
+                        play.setImageResource(R.drawable.stop);
+                    }
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    System.out.println("接听");
+                    if(mediaPlayer!=null &&mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        isPlaying = true;
+                        play.setImageResource(R.drawable.play);
+                    }
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:
+                    System.out.println("响铃:来电号码" + incomingNumber);
+                    if(mediaPlayer!=null &&mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        isPlaying = true;
+                        play.setImageResource(R.drawable.play);
+                    }
+                    //输出来电号码
+                    break;
+            }
+            super.onCallStateChanged(state, incomingNumber);
+        }
+    };
+
+
+    private void initList(){
+        musicInfos = new ArrayList<>();
+
+        MusicLoader musicLoader =  MusicLoader.instance(this.getContentResolver());
+        musicInfos = musicLoader.getMusicList();
+
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return musicInfos.size();
+            }
+
+            @Override
+            public MusicLoader.MusicInfo getItem(int position) {
+                return musicInfos.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = LayoutInflater.from(MainActivity.this).inflate(android.R.layout.simple_list_item_2, null);
+                }
+                MusicLoader.MusicInfo item = getItem(position);
+                TextView textView = (TextView) convertView.findViewById(android.R.id.text1);
+                textView.setText(item.getTitle().replace(".mp3", "").replace(".flac", "").replace(".acc", ""));
+                TextView textView1 = (TextView) convertView.findViewById(android.R.id.text2);
+                textView1.setText(TimeUtils.intToString(item.getDuration() / 1000) + " | " +
+                        item.getArtist());
+                return convertView;
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                playMusic(position);
+            }
+        });
+    }
+
+
+    private void initPlayView(){
+        previous = (ImageView) findViewById(R.id.previous);
+        play = (ImageView) findViewById(R.id.play);
+        next = (ImageView) findViewById(R.id.next);
+        playnow = (TextView) findViewById(R.id.playnow);
+        playtime = (TextView) findViewById(R.id.playtime);
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setEnabled(false);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if(mediaPlayer!=null){
+                    mediaPlayer.seekTo(seekBar.getProgress());
+                }
+            }
+        });
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playMusic((playId++) % musicInfos.size());
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playId == 0)
+                    playId = musicInfos.size();
+                playMusic((playId--) % musicInfos.size());
+            }
+        });
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                    play.setImageResource(R.drawable.play);
+                    mediaPlayer.pause();
+                    isPlaying = false;
+                } else if (mediaPlayer != null) {
+                    play.setImageResource(R.drawable.stop);
+                    mediaPlayer.start();
+                    isPlaying = true;
+                } else {
+                    play.setImageResource(R.drawable.stop);
+                    playMusic(playId);
+                }
+            }
+        });
+    }
+
+
+    private void playMusic(int position){
+        if(position > musicInfos.size())
+            return;
+        SPUtils.put(this, SPTAG, position);
+        if(mediaPlayer!=null&&mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer = null;
+        }
+        mediaPlayer = new MediaPlayer();
+        try {
+            play(position);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void play(final int position) throws IOException {
+        seekBar.setEnabled(false);
+        isPlaying = false;
+        mediaPlayer.setDataSource(musicInfos.get(position).getUrl());
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setLooping(false);
+        mediaPlayer.prepareAsync();
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                playnow.setText(musicInfos.get(position).getTitle().replace(".mp3", "")
+                        .replace(".flac", "").replace(".acc", ""));
+                mp.start();
+                isPlaying = true;
+                play.setImageResource(R.drawable.stop);
+                seekBar.setMax(mp.getDuration());
+                seekBar.setEnabled(true);
+                startProgressLoading();
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playMusic((playId++) % musicInfos.size());
+            }
+        });
+        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                mp.reset();
+                return false;
+            }
+        });
+    }
+
+    private void startProgressLoading(){
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(mediaPlayer!=null&&mediaPlayer.isPlaying())
+                    handleProgress.sendEmptyMessage(0);
+            }
+        }, 0, 1000);
+    }
+
+    Handler handleProgress = new Handler() {
+        public void handleMessage(Message msg) {
+            seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            playtime.setText(TimeUtils.intToString(mediaPlayer.getCurrentPosition()/1000));
+        };
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer!=null&&mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer = null;
+        }
+        mediaPlayer.release();
+        super.onDestroy();
+    }
+
+    /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
---
+++ app/src/main/java/com/cwf/app/musicplay/TimeUtils.java
@@ -0,0 +1,126 @@
+package com.cwf.app.musicplay;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtils {
+	static SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	/**
+	 * yyyy-MM-dd HH:mm:ss
+	 * @return
+	 */
+	public static SimpleDateFormat getSimpleDateFormat(){
+		return simpleDateFormat;
+	}
+	
+	
+	
+	
+	public static SimpleDateFormat getSimpleDateFormat(String format){
+		return new SimpleDateFormat(format);
+	}
+	
+	/**
+	 * 获得当前时间  按照格式：formatString
+	 * @return
+	 */
+	public static String getSimpleDate(String formatString){
+		return new SimpleDateFormat(formatString).format(new Date());
+	}
+	
+	/**
+	 * 获得当前时间  按照格式：yyyy-MM-dd HH:mm:ss
+	 * @return
+	 */
+	public static String getSimpleDate(){
+		return getSimpleDateFormat().format(new Date());
+	}
+	
+	/**
+	 * 获得当前时间  按照格式：yyyy-MM-dd HH:mm:ss
+	 * @return
+	 */
+	public static String getSimpleDate(Date date){
+		return getSimpleDateFormat().format(date);
+	}
+
+	/**
+	 * 获得2个时间的间隔秒数
+	 * @param old_date 旧时间
+	 * @param new_date 新时间
+	 * @return
+	 */
+	public static long getIntervalTime(Date old_date, Date new_date) {
+		long second = 0;
+		try {
+			// 得到间隔秒数
+			second = (new_date.getTime() - old_date.getTime());
+		} catch (Exception e) {
+			return 0;
+		}
+		return second;
+	}
+	
+	/**
+	 * 比较时间大小、先后，
+	 * time1比time2晚返回1
+	 * time1比time2早返回-1
+	 * 一样返回0
+	 * @param time1
+	 * @param time2
+	 * @return
+	 * @throws ParseException
+	 */
+	public static int dateTimeCompara(String time1, String time2)
+			throws ParseException {
+		Date dt1 = simpleDateFormat.parse(time1);
+		Date dt2 = simpleDateFormat.parse(time2);
+		if (dt1.getTime() > dt2.getTime()) {
+//			System.out.println("dt1 在dt2前");
+			return 1;
+		} else if (dt1.getTime() < dt2.getTime()) {
+//			System.out.println("dt1在dt2后");
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+	/*计算视频的时长，返回hh:mm:ss格式的时间
+	* time 单位为秒*/
+	public static String intToString(int time){
+		String str = "";
+		if(time > 0){
+			int hours = time / 3600;
+			int minutes = (time - hours * 3600) / 60;
+			int second = time % 60;
+			if(hours >0)
+				str = hours + ":";
+			if(minutes >0 || hours>0) {
+				if (minutes < 10) {
+					if(minutes == 0)
+						str = str + "00:";
+					else
+						str = str + "0" + minutes + ":";
+				}
+				else
+					str = str + hours + ":";
+			}else
+				str = "00:";
+			if(second > 0 || minutes> 0){
+				if(second < 10){
+					if(second == 0)
+						str = str + "00";
+					else
+						str = str + "0" + second;
+				}
+				else
+					str = str + second;
+			}
+
+		}
+
+		return str;
+	}
+}
